c30c716241faa8e19e96d63385bb9143ea96c872
sergey <paksergey94@gmail.com>
2020-01-24 12:21:39 +1000

    fix Уникальные просмотры

--- frontend/services/ReceiptViewRegister.php
+++ frontend/services/ReceiptViewRegister.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace frontend\services;
 
 use common\models\ReceiptViewsCount;
+use Yii;
+use yii\web\Cookie;
 
 /**
  * Регистрация просмотра рецепта.
@@ -13,6 +15,8 @@ use common\models\ReceiptViewsCount;
  */
 class ReceiptViewRegister {
 
+    const REGISTERED_RECEIPTS_COOKIE_NAME = 'registered:receipts';
+
     /**
      * @param int $id Идентификатор рецепта
      *
@@ -20,9 +24,21 @@ class ReceiptViewRegister {
      */
     public function register(int $id) {
         //todo-23.01.20-pak.s Переделать на аякс и редис когда-нибудь
+        $receiptsIds = Yii::$app->request->cookies->getValue(static::REGISTERED_RECEIPTS_COOKIE_NAME, []);
+
+        if (in_array($id, $receiptsIds)) {
+            return;
+        }
+
+        $receiptsIds[] = $id;
+        Yii::$app->response->cookies->add(new Cookie([
+            'name'  => static::REGISTERED_RECEIPTS_COOKIE_NAME,
+            'value' => $receiptsIds,
+        ]));
+
         $count = ReceiptViewsCount::findOne([ReceiptViewsCount::ATTR_RECEIPT_ID => $id]);
         if (null === $count) {
-            $count = new ReceiptViewsCount();
+            $count             = new ReceiptViewsCount();
             $count->receipt_id = $id;
         }
         $count->count++;
