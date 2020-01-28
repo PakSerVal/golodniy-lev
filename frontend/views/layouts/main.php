137eb5240f9cf702083536a58528b0a78382affa
sergey <paksergey94@gmail.com>
2020-01-28 23:34:20 +1000

    fix title

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -17,7 +17,7 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title ?? 'Голодный лев') ?></title>
+    <title><?= Html::encode($this->title ?? 'Голодный лев. Рецепты, доступные каждому') ?></title>
     <?php $this->head() ?>
 </head>
 <body>
