caa0cbe789c0ba0fba95c3a73f9dcae3e274d0a8
sergey <paksergey94@gmail.com>
2020-01-29 13:32:50 +1000

    fix header and fonts

--- frontend/views/layouts/blocks/header.php
+++ frontend/views/layouts/blocks/header.php
@@ -12,7 +12,7 @@ use yii\helpers\Html;
         <div class="header__search">
             <form method="get" action="/search" class="search-form">
                 <div class="presearch-input-wrapper">
-                    <input id="presearch-input" type="text" name="q" autocomplete="off" data-presearch-url="/presearch" class="search-form__input" placeholder="Поиск по сайту...">
+                    <input id="presearch-input" type="text" name="q" autocomplete="off" data-presearch-url="/presearch" class="search-form__input" placeholder="Поиск рецептов...">
                     <i data-search-icon><?= Html::img('/images/icons/search.png', ['alt' => 'search']) ?></i>
                 </div>
                 <div class="presearch" data-search-wrapper></div>
@@ -20,7 +20,7 @@ use yii\helpers\Html;
             <div class="header__media">
                 <a href="https://www.instagram.com/golodnyilev"><?= Html::img('/images/icons/instagram.png') ?><span>Инстаграм</span></a>
                 <a href="https://www.youtube.com/channel/UC1eNb9DPsG4rgKBmREhIsWQ"><?= Html::img('/images/icons/youtube.png') ?><span>Youtube</span></a>
-                <a href="https://vk.com/golodnyilev"><?= Html::img('/images/icons/vk.png') ?><span>Вконтакте</span></a>
+                <a class="vk" href="https://vk.com/golodnyilev"><?= Html::img('/images/icons/vk.png') ?><span>Вконтакте</span></a>
             </div>
         </div>
     </div>
