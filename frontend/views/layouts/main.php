48db26de969b762787ee33ed4ba076dacd4a3532
sergey <paksergey94@gmail.com>
2020-01-29 13:44:23 +1000

    yandex metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -19,6 +19,25 @@ AppAsset::register($this);
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title ?? 'Голодный лев. Рецепты, доступные каждому') ?></title>
     <?php $this->head() ?>
+
+    <?php if (!YII_ENV_DEV): ?>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(57248095, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/57248095" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+    <?php endif; ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
