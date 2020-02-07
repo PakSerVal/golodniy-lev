113af69550bea492700ec488c3456fde7f73d807
sergey <paksergey94@gmail.com>
2020-02-07 23:34:48 +1000

    gif

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -5,4 +5,4 @@
 $this->title = 'Админка!!!!!!!!';
 ?>
 <h1>Привет Лёва!</h1>
-<iframe src="https://giphy.com/embed/NZJWY9fcr5Qkg" width="480" height="400" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dog-lol-humor-NZJWY9fcr5Qkg">via GIPHY</a></p>
+<iframe src="https://giphy.com/embed/cLAJpbqLnVAIUDhfx9" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
